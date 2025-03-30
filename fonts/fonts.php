a1dce9236ee9336eb1a0a9cfb9a06f53ca50afe4
Sean Murphy <sean@iamseanmurphy.com>
2025-03-30 05:18:05 +0000

    2.8

--- fonts/fonts.php
+++ fonts/fonts.php
@@ -3,7 +3,7 @@
 Plugin Name: Fonts
 Plugin URI: https://wpsites.net/best-plugins/plugin-fonts-styles-sizes-wordpress/
 Description: Premium Upgrades: <a href="http://wpsites.net/wordpress-themes/add-custom-fonts-to-the-wordpress-editor/">Add Google & Custom Fonts</a> | <a href="https://wordpress.org/support/plugin/fonts/reviews/?filter=5">Click here to leave a review</a>
-Version: 2.7
+Version: 2.8
 Author: Brad Dalton - WP Sites
 Author URI: https://wpsites.net/bradley-james-dalton-wordpress-developer/
 License: GPL2
@@ -18,7 +18,7 @@ add_action('admin_menu', 'premium_custom_fonts_menu');
 function premium_custom_fonts_menu() {
     add_menu_page(
         'Fonts', // Page title
-        'Fonts', // Menu title
+        'Fonts Pro', // Menu title
         'manage_options', // Capability
         'custom-fonts', // Menu slug
         'custom_fonts_page_content', // Callback function
@@ -30,25 +30,31 @@ function premium_custom_fonts_menu() {
 function custom_fonts_page_content() {
     ?>
     <div class="wrap">
-        <h1>How To Install Custom & Google Fonts</h1>
+        <h1>Fonts</h1>
         
-        <!-- Button -->
-
-		<p><a class="button button-primary" href="https://wpsites.net/product/custom-fonts-for-your-visual-editor-in-wordpress/">
-		Get Custom & Google Fonts Extension
-		</a></p>
-		
-		 <!-- Paragraph -->
-        <p style="margin-top: 15px; font-size: 16px;">Works the same way for uploading Custom Fonts and Google Font files.</p>
-       
-        <?php echo '<iframe width="100%" height="600px" src="https://www.youtube.com/embed/8ERYNm_2oPI?si=MGe2b8kkPrdQ8lxc&rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'; ?>
-        
-        <p><a class="button button-primary" href="https://wordpress.org/support/plugin/fonts/reviews/?filter=5">Click here to leave a review</a></p>
-        
+        <div class="custom-fonts-instructions" style="font-size: 16px;">
+            <h2>Upgrade to Fonts Pro</h2>
+            <ol style="margin-left: 20px;">
+                <li style="margin-bottom: 15px;"><strong>Install Fonts Pro</strong>
+                    <ol style="margin-left: 20px; margin-top: 10px;">
+                        <li>Purchase and download <a href="https://wpsites.net/product/custom-fonts-for-your-visual-editor-in-wordpress/">Fonts Pro</a></li>
+                        <li>Install and activate the plugin through WordPress plugin installer</li>
+                    </ol>
+                </li>
+                <li style="margin-bottom: 15px;"><strong>Upload Font Files</strong>
+                    <ol style="margin-left: 20px; margin-top: 10px;">
+                        <li>Go to the Custom Fonts menu in your WordPress dashboard</li>
+                        <li>Upload your custom font files (supports WOFF, WOFF2, TTF, OTF)</li>
+                        <li>Add the Font Family Name alongside the file name</li>
+                        <li>Save your changes</li>
+                    </ol>
+                </li>
+            </ol>
+            <p style="margin-top: 20px;">Ready to use custom fonts? <a href="https://wpsites.net/product/custom-fonts-for-your-visual-editor-in-wordpress/">Get Fonts Pro now</a></p>
+        </div>
     </div>
     <?php
 }
-
 
 function add_more_buttons($buttons) {
 $buttons[] = 'fontselect';
@@ -60,12 +66,12 @@ add_filter("mce_buttons_3", "add_more_buttons");
 
 add_action('admin_notices', 'wpsites_fonts_plugin_notice');
 function wpsites_fonts_plugin_notice() {
-    global $current_user ;
+    global $current_user;
         $user_id = $current_user->ID;
         /* Check that the user hasn't already clicked to ignore the message */
     if ( ! get_user_meta($user_id, 'example_ignore_notice') ) {
         echo '<div class="updated"><p>'; 
-        printf(__('Please support fonts by leaving a review : <a href="https://wordpress.org/support/plugin/fonts/reviews/?filter=5">Click here to leave a quick review</a>  | <a href="%1$s">Hide Notice</a>'), '?example_nag_ignore=0');
+        printf(__('Please support Fonts by leaving a review : <a href="https://wordpress.org/support/plugin/fonts/reviews/?filter=5">Click here to leave a quick review</a>  | <a href="%1$s">Hide Notice</a>'), '?example_nag_ignore=0');
         echo "</p></div>";
     }
 }
--- fonts/readme.txt
+++ fonts/readme.txt
@@ -2,9 +2,9 @@
 Contributors: wordpresssites
 Tags: fonts,font plugin, editor fonts, custom fonts, google fonts
 Requires at least: 4.0
-Tested up to: 6.7
+Tested up to: 6.7.2
 License: GPLv2 or later
-Stable tag: 3.9.0
+Stable tag: 2.8
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 Add More Font Styles & Sizes To Your Visual Editor in WordPress
@@ -16,28 +16,22 @@ This plugin adds 2 drop down menus to your visual editor with additional sizes a
 1. A button for Styles
 2. A button for Sizes
 
-New: You can also add your own selection of <a href="https://wpsites.net/product/custom-fonts-for-your-visual-editor-in-wordpress/">Google & Custom fonts</a> including premium fonts, to your editor by purchasing a premium upgrade.
+New: You can also add your own selection of <a href="https://wpsites.net/product/custom-fonts-for-your-visual-editor-in-wordpress/">Google & Custom fonts</a> including premium fonts, to your editor by upgrading to Fonts Pro.
 
-<p><strong>Premium Upgrades:</strong></p>
-<ul>
-<li><a href="https://wpsites.net/product/custom-fonts-for-your-visual-editor-in-wordpress/">Google & Custom Fonts</a></li>
-</ul>
-
-== Video ==
-
-Shows you how to add Google & Custom Fonts
-
-https://youtu.be/8ERYNm_2oPI
+<p><strong>Fonts Pro:</strong></p>
+<ol>
+<li><a href="https://wpsites.net/product/custom-fonts-for-your-visual-editor-in-wordpress/">Fonts Pro</a></li>
+</ol>
 
 == Support ==
 
 New <a href="https://wpsites.net/contact">Support</a>
-== Gutenberg ==
+
+== Gutenberg ==
 
 Works with the latest version of Gutenberg
 
 == Screenshots ==
 
 1. This is the Font Family menu
-
 2. This is the Font Sizes menu
